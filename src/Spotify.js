892daac13163b7c121fc3e865dd2a984f4fd6d9e
oc34n <ocean.wrng@gmail.com>
2023-06-20 19:27:49 -0400

    Fixed redirect uri

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-export const REDIRECT_URI = "https://soundstack.netlify.app/";
+export const REDIRECT_URI = "https://soundstack.netlify.app";
 export const AUTH_ENDPOINT = process.env.REACT_APP_AUTH_ENDPOINT;
 export const RESPONSE_TYPE = "token";
