d66c2b9e424313c263a12eb6ba441df201c83e28
oc34n <ocean.wrng@gmail.com>
2023-06-20 21:02:29 -0400

    Implemented NetlifyCLI

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
         CLIENT_SECRET,
     };
 
-    fetch("https://accounts.spotify.com/api/token", authParams)
+    await fetch("https://accounts.spotify.com/api/token", authParams)
       .then((result) => result.json())
       .then((data) => setAccessToken(data.access_token));
   }, []);
